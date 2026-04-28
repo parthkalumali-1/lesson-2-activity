7223c8223ff407e7858654ee6368f270d03ad6aa
parth <parthkalumali@gmail.com>
2026-04-28 22:36:13 +0530

    second commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("=== Birthday List ===\n")
+
+raj = "Raj - 12 Jan"
+amit = "Amit - 5 Feb"
+rohan = "Rohan - 18 March"
+sneha = "Sneha - 22 July"
+priya = "Priya - 30 Dec"
+
+print(raj)
+print(amit)
+print(rohan)
+print(sneha)
+print(priya)
